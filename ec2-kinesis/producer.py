b96c393b63dfb5296c28f6436616c2b586b34d87
Ben Salem Meryem <124097610+MeryemBenSalem@users.noreply.github.com>
2025-05-19 11:18:57 +0100

    Update producer.py

--- ec2-kinesis/producer.py
+++ ec2-kinesis/producer.py
@@ -7,10 +7,10 @@ def send_image_to_kinesis(file_path):
     with open(file_path, "rb") as f:
         encoded = base64.b64encode(f.read()).decode('utf-8')
 
-    kinesis = boto3.client('kinesis', region_name='eu-west-1')
+    kinesis = boto3.client('kinesis', region_name='us-east-1')
     kinesis.put_record(StreamName='TrafficDataStream', Data=encoded, PartitionKey='1')
 
-traci.start(["sumo-gui", "-c", "your.sumocfg"])
+traci.start(["sumo-gui", "-c", ""]) #Config file 
 for step in range(1000):
     traci.simulationStep()
     if step % 50 == 0:  # every ~5 seconds
